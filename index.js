8ed7849c03b28f9a6cee3ac4baed06bbf88cd3be
Matthew <cameron.matt@outlook.com>
2021-03-11 18:54:31 +1100

    done find lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(record) {
+    const found = record.find( ({ result }) => result === "W");
+    if (found != undefined) {
+        return found.year;
+    } else {
+        return undefined;
+    }
+}
